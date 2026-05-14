fed2fd9f2bbe55b3d8069a83bf3c67607b8e573d
Jothika <raviravi31799@gmail.com>
2026-05-14 17:08:26 +0530

    Added ForgetPassword feature

---
+++ src/test/java/com/pages/ForgetPasswordPage.java
@@ -0,0 +1,26 @@
+package com.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ForgetPasswordPage extends BasePage {
+	public ForgetPasswordPage(WebDriver driver) {
+          super(driver);
+    }
+
+	@FindBy(xpath="//div[@class='form-group']//a[normalize-space()='Forgotten Password']")
+	public WebElement forgetpassword;
+	
+	@FindBy(xpath="//input[@id='input-email']")
+	public WebElement email;
+	
+	@FindBy(xpath="//button[normalize-space()='Continue']")
+	public WebElement button;
+	
+	@FindBy(xpath="//div[contains(@class,'alert-success')]")
+	public WebElement message;
+
+	@FindBy(xpath="//div[contains(@class,'alert-danger')]")
+	public WebElement warningmsg;
+}
---
+++ src/test/java/com/stepDefinitions/Forgetpassword.java
@@ -0,0 +1,80 @@
+package com.stepDefinitions;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import com.actions.ForgetpasswordPageAction;
+import com.actions.LoginPageAction;
+import com.driver.DriverClass;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Forgetpassword {
+	WebDriver driver = DriverClass.getDriver();
+	ForgetpasswordPageAction fpa=new ForgetpasswordPageAction();
+	LoginPageAction lpa=new LoginPageAction();
+	private static final Logger log = LogManager.getLogger(Forgetpassword.class);
+	
+	@Given("the user is on the home page of the application")
+	public void the_user_is_on_the_home_page_of_the_application() {
+		 lpa.launchWebUrl();
+	}
+
+	@When("the user clicks login link")
+	public void the_user_clicks_login_link() {
+		 lpa.clickMyAccountLink();
+	}
+
+	@When("clicks on Forgotten Password")
+	public void clicks_on_forgotten_password() {
+		fpa.clickForgotPassword();
+	}
+
+	@When("the user enters the valid {string}")
+	public void the_user_enters_the_valid(String email) {
+		fpa.enterEmail(email);
+		fpa.clickContinueButton();
+	}
+
+	@Then("the user should be able to receive a {string} stating reset link sent to email.")
+	public void  the_user_should_be_able_to_receive_a_stating_reset_link_sent_to_email(String message) {
+		String actual=fpa.Successmsg();
+	   try{
+		   
+		 Assert.assertEquals(actual,message);
+		 log.info("Reset link sent to email");
+	    }
+	   catch(AssertionError e){
+		   log.error("Failed to sent email"+e.getMessage());
+	   }
+		   
+	   
+	}
+
+	@When("the user enters the invalid {string}")
+	public void the_user_enters_the_invalid(String invalidemail){
+		fpa.enterEmail(invalidemail);
+		fpa.clickContinueButton();
+	}
+
+	@Then("the user should receive an {string}")
+	public void the_user_should_receive_an(String error_message) {
+		String actual=fpa.Warningmsg();
+		   try{
+			   
+			 Assert.assertEquals(actual,error_message);
+			 log.info("Failed to send reset link to invalid email");
+		    }
+		   catch(AssertionError e ){
+			   log.error("Failed to display warning message"+e.getMessage());
+		   }
+			   
+	}
+
+
+
+}
