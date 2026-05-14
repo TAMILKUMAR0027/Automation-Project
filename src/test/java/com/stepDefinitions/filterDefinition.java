4b4ad0b34f6e28db7fb15d5d86b93fecbd9ccf6e
TAMILKUMAR0027 <tamilkumar0027@gmail.com>
2026-05-14 14:53:58 +0530

    Conflict resolved

--- src/test/java/com/stepDefinitions/filterDefinition.java
+++ src/test/java/com/stepDefinitions/filterDefinition.java
@@ -14,46 +14,47 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class filterDefinition {
+
 	LaunchPages lp = new LaunchPages(DriverClass.getDriver());
-	filterPageAction fpa=new filterPageAction();
-	productPageAction ppa=new productPageAction();
-	LaunchPageAction lpa=new LaunchPageAction();
+
+	filterPageAction fpa = new filterPageAction();
+
+	productPageAction ppa = new productPageAction();
+
+	LaunchPageAction lpa = new LaunchPageAction();
+
 	@Given("User is on Home page click on the Shop By categories Page and Click any one option")
 	public void user_is_on_home_page_click_on_the_shop_by_categories_page_and_click_any_one_option() {
-	    // Write code here that turns the phrase above into concrete actions
-		 try {
-	            String url = ConfigReader.getProperties().getProperty("url");
-	          
 
-	            lp.launchApplication(url);
+		try {
 
-	        } catch (Exception e) {
-	            
-	            throw e;
-	        }
+			String url = ConfigReader.getProperties().getProperty("url");
+
+			lp.launchApplication(url);
+
+		} catch (Exception e) {
+
+			throw e;
+		}
 	}
 
 	@When("User clicks manufacturer filter any brand element")
 	public void user_clicks_manufacturer_filter_any_brand_element() {
-	    // Write code here that turns the phrase above into concrete actions
-	    lpa.clickCategories();
-	    fpa.clickManufacture();
-	    
+
+		lpa.clickCategories();
+
+		fpa.clickManufacture();
 	}
 
 	@When("product should be displayed based on filtered results and Clicks any one product")
 	public void product_should_be_displayed_based_on_filtered_results_and_clicks_any_one_product() {
-	    // Write code here that turns the phrase above into concrete actions
+
 		fpa.clickProduct();
 	}
 
 	@Then("check the product brand name in description should be matches the filter")
 	public void check_the_product_brand_name_in_description_should_be_matches_the_filter() {
-	    // Write code here that turns the phrase above into concrete actions
-	    Assert.assertEquals("Apple",ppa.getBrandName());
+
+		Assert.assertEquals(ppa.getBrandName(), "Apple");
 	}
-
-
-
-
 }
