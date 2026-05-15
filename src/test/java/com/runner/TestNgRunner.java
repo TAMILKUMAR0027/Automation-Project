44927fcc42193d343e00d7bfaf99e9784544ef58
Rishwanth Adhishwar <krishwanthadhiswar@gmail.com>
2026-05-15 10:21:48 +0530

    Add tags for Login and Register features

--- src/test/java/com/runner/TestNgRunner.java
+++ src/test/java/com/runner/TestNgRunner.java
@@ -7,6 +7,7 @@ import org.testng.annotations.DataProvider;
 @CucumberOptions(
 		features = "src/test/resources/features/",
 		glue = {"com.stepDefinitions", "com.hooks"},
+	    tags= "@LoginFeature or @RegisterFeature",
 		plugin = {
 				"pretty",
 				"html:target/cucumber-report.html",
