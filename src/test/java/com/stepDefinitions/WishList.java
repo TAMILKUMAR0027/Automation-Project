da509912f1d7367d6a6148914f65b28252c5033e
14-Prasanna <2k22cse115@kiot.ac.in>
2026-05-15 16:34:33 +0530

    Added the new features

---
+++ src/test/java/com/stepDefinitions/WishList.java
@@ -0,0 +1,87 @@
+package com.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class WishList {
+
+    @Given("The user is on the home page of Website")
+    public void the_user_is_on_the_home_page_of_website() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("the user navigate to Top Products section")
+    public void the_user_navigate_to_top_products_section() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user click the iMac product and add to the wishlist")
+    public void the_user_click_the_i_mac_product_and_add_to_the_wishlist() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the product should be added to the wishList")
+    public void the_product_should_be_added_to_the_wish_list() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the user clicks wishList button")
+    public void the_user_clicks_wish_list_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the application should navigate to the MyWishList page")
+    public void the_application_should_navigate_to_the_my_wish_list_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user adds multiple products to the wishlist")
+    public void the_user_adds_multiple_products_to_the_wishlist() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("all selected products should be displayed in the wishList page")
+    public void all_selected_products_should_be_displayed_in_the_wish_list_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should see correct product name {string}")
+    public void the_user_should_see_correct_product_name(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should see correct product price")
+    public void the_user_should_see_correct_product_price() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should see correct stock status")
+    public void the_user_should_see_correct_stock_status() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user removes the product from wishlist")
+    public void the_user_removes_the_product_from_wishlist() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the product should be removed successfully from wishList")
+    public void the_product_should_be_removed_successfully_from_wish_list() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+
+
+
+}
