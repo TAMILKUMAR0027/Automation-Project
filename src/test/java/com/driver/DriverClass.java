6a5840b2463b81b0ad77a3841105cf4f87d31018
samihamuhabathulla-jpg <samihamuhabathulla@gmail.com>
2026-05-18 14:42:58 +0530

    Update DriverClass.java

--- src/test/java/com/driver/DriverClass.java
+++ src/test/java/com/driver/DriverClass.java
@@ -2,18 +2,22 @@ package com.driver;
 
 import com.utils.ConfigReader;
 import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverClass {
-	
 
     private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
     private static final Logger logger = LogManager.getLogger(DriverClass.class);
 
     public static WebDriver getDriver() {
@@ -24,48 +28,69 @@ public class DriverClass {
     public static void initDriver() {
 
         String browser = ConfigReader.getProperties().getProperty("browser");
+
         String headlessValue = ConfigReader.getProperties().getProperty("headless");
 
-        boolean headless = headlessValue != null && headlessValue.equalsIgnoreCase("true");
+        boolean headless = headlessValue != null
+                && headlessValue.equalsIgnoreCase("true");
 
         logger.info("Initializing Browser: " + browser);
+
         logger.info("Headless Mode: " + headless);
 
         if (browser.equalsIgnoreCase("chrome")) {
 
             WebDriverManager.chromedriver().setup();
+
             ChromeOptions options = new ChromeOptions();
 
             if (headless) {
+
                 options.addArguments("--headless=new");
             }
+
+            options.addArguments("--remote-allow-origins=*");
+
+            options.addArguments("--disable-dev-shm-usage");
+
+            options.addArguments("--no-sandbox");
 
             driver.set(new ChromeDriver(options));
 
-        } else if (browser.equalsIgnoreCase("firefox")) {
+        }
+
+        else if (browser.equalsIgnoreCase("firefox")) {
 
             WebDriverManager.firefoxdriver().setup();
+
             FirefoxOptions options = new FirefoxOptions();
 
-            if (headless) {
-                options.addArguments("--headless=new");
-            }
+            options.setHeadless(headless);
 
             driver.set(new FirefoxDriver(options));
 
-        } else {
+        }
+
+        else {
+
             logger.error("Invalid Browser Name: " + browser);
+
             throw new RuntimeException("Invalid Browser Name: " + browser);
         }
 
         getDriver().manage().window().maximize();
+
         logger.info("Browser launched successfully");
     }
 
     public static void quitDriver() {
+
         if (getDriver() != null) {
+
             logger.info("Closing browser session");
+
             getDriver().quit();
+
             driver.remove();
         }
     }
