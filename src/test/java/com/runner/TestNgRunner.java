be8ec1b61557ab718768cca6ae0f612bdd4ccae2
Rishwanth Adhishwar <krishwanthadhiswar@gmail.com>
2026-05-19 20:21:02 +0530

    Add tags for feature tests in TestNgRunner

--- src/test/java/com/runner/TestNgRunner.java
+++ src/test/java/com/runner/TestNgRunner.java
@@ -15,6 +15,8 @@ import org.testng.annotations.DataProvider;
                         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                         "rerun:target/failedrerun.txt"
                 },
+
+	           tags="@RegisterFeature or @LoginFeature or @CartFeature",
 	            
 
                 monochrome = true
