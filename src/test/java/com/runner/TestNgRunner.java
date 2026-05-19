77f195f952d6dae4d2804301dd8b376cf6a782b9
Rishwanth Adhishwar <krishwanthadhiswar@gmail.com>
2026-05-19 20:14:30 +0530

    Update TestNgRunner.java

--- src/test/java/com/runner/TestNgRunner.java
+++ src/test/java/com/runner/TestNgRunner.java
@@ -15,6 +15,7 @@ import org.testng.annotations.DataProvider;
                         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                         "rerun:target/failedrerun.txt"
                 },
+	            tags= "@LoginFeature or @RegisterFeature or @CartFeature",
 
                 monochrome = true
         )
