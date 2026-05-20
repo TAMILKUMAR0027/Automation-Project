1e310d3842032bd1e2f85b3d65b5afb8e066df55
Prasanna venkatesh <168177926+14-Prasanna@users.noreply.github.com>
2026-05-20 17:19:35 +0530

    Update tags in TestNgRunner for broader test coverage

--- src/test/java/com/runner/TestNgRunner.java
+++ src/test/java/com/runner/TestNgRunner.java
@@ -15,7 +15,7 @@ import org.testng.annotations.DataProvider;
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "rerun:target/failedrerun.txt",
         },
-		tags = "@Search",
+		tags = "@Search or @WishlistFeature or @Smoke",
         monochrome = true)
 public class TestNgRunner extends AbstractTestNGCucumberTests {
 	
