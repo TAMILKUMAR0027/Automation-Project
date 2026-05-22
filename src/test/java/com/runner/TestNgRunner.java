af3572e157a8d34c647b9ad7972708e6505635e6
Prasanna venkatesh <168177926+14-Prasanna@users.noreply.github.com>
2026-05-22 10:08:20 +0530

    Update TestNgRunner.java

--- src/test/java/com/runner/TestNgRunner.java
+++ src/test/java/com/runner/TestNgRunner.java
@@ -15,7 +15,7 @@ import org.testng.annotations.DataProvider;
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "rerun:target/failedrerun.txt",
         },
-		tags = "@Search or @WishlistFeature or @Smoke",
+		tags = "@Search",
         monochrome = true)
 public class TestNgRunner extends AbstractTestNGCucumberTests {
 	
