0dd95c6621cfad02cd5851a86fdecb43e1b9639d
TAMIL KUMAR E <153970943+TAMILKUMAR0027@users.noreply.github.com>
2026-05-25 16:29:28 +0530

    Update TestNgRunner.java

--- src/test/java/com/runner/TestNgRunner.java
+++ src/test/java/com/runner/TestNgRunner.java
@@ -16,7 +16,7 @@ import org.testng.annotations.DataProvider;
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 
                 "rerun:target/failedrerun.txt"
-    },
+    },tags="@Tamil",
      
         monochrome = true
 ) 
