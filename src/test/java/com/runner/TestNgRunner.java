599a020c7fd8a5fdf4e4838b8ed01cb296a94fcf
Prasanna venkatesh <168177926+14-Prasanna@users.noreply.github.com>
2026-05-31 13:21:26 +0530

    Update TestNgRunner.java

--- src/test/java/com/runner/TestNgRunner.java
+++ src/test/java/com/runner/TestNgRunner.java
@@ -4,13 +4,21 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-@CucumberOptions(features = "src/test/resources/features/", glue = { "com.stepDefinitions", "com.hooks" }, plugin = {
-		"pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json",
-		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-		"rerun:target/failedrerun.txt" }, 
+@CucumberOptions(
 
-		monochrome = true)
+		features = "src/test/resources/features/", 
+		glue = { "com.stepDefinitions", "com.hooks" }, 
+		plugin = { "pretty",
+				"html:target/cucumber-report.html", "json:target/cucumber-report.json",
+				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				"rerun:target/failedrerun.txt" },
+		
+	
+        monochrome = true
+)
+
+
 
 public class TestNgRunner extends AbstractTestNGCucumberTests {
 	@Override
