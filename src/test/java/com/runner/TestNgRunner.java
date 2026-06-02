f2150530c06f898ee945561329db6726a517c2fc
TAMIL KUMAR E <153970943+TAMILKUMAR0027@users.noreply.github.com>
2026-06-02 16:53:47 +0530

    Update TestNgRunner.java

--- src/test/java/com/runner/TestNgRunner.java
+++ src/test/java/com/runner/TestNgRunner.java
@@ -11,7 +11,7 @@ import org.testng.annotations.DataProvider;
 				"html:target/cucumber-report.html", "json:target/cucumber-report.json",
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-				"rerun:target/failedrerun.txt" },
+				"rerun:target/failedrerun.txt" },tags="@Tamil",
 
 
         monochrome = true
