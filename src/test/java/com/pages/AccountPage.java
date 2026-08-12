b819aefe8954d723123c18f1909b1c309d128b1c
Rishwanth Adhishwar <krishwanthadhiswar@gmail.com>
2026-08-12 17:52:58 +0530

    Update AccountPage.java

--- src/test/java/com/pages/AccountPage.java
+++ src/test/java/com/pages/AccountPage.java
@@ -58,7 +58,7 @@ public class AccountPage extends BasePage {
 	@FindBy (xpath="//div[@id='account-account']/child::div[1]")
 	public WebElement sucessMsgNL;
 	
-	@FindBy(xpath = "//a[@class='list-group-item'][normalize-space()='Address Book']")
+	@FindBy(xpath = "//a[normalize-space()='Address Book']/i")
 	public WebElement AddressBook;
 	
 	@FindBy(xpath = "//a[@class='btn btn-primary']")
