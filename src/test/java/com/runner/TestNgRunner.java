1ff9fd1abc54b3f3f1363b622c66859bfabac8b6
Rishwanth Adhishwar <krishwanthadhiswar@gmail.com>
2026-08-12 13:15:41 +0530

    Update Cucumber tags in TestNgRunner

--- src/test/java/com/runner/TestNgRunner.java
+++ src/test/java/com/runner/TestNgRunner.java
@@ -9,7 +9,7 @@ import org.testng.annotations.DataProvider;
 		"pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json",
 		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-		"rerun:target/failedrerun.txt" },monochrome = true,tags=" @RewardPoint")
+		"rerun:target/failedrerun.txt" },monochrome = true,tags=" @Rishwanth")
 
 
 
