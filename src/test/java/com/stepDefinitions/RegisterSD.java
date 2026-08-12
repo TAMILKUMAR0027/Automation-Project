377621909a6ba998e0956d7c5bbd5179cc8b6239
Rishwanth Adhishwar <krishwanthadhiswar@gmail.com>
2026-08-12 14:07:28 +0530

    Added Dynamic Email Generation While Registering And Changes In Locators On adding New Address Drop Down

--- src/test/java/com/stepDefinitions/RegisterSD.java
+++ src/test/java/com/stepDefinitions/RegisterSD.java
@@ -74,7 +74,7 @@ public class RegisterSD {
 		String pass = ConfigReader.getRegisterDataProperties().getProperty("pass");
 		String cpass = ConfigReader.getRegisterDataProperties().getProperty("cpass");
 		rpa.setLname(lname);
-		rpa.setEmail(email);
+		rpa.setDynamicEmail();
 		rpa.setTelephone(telephone);
 		rpa.setPassword(pass);
 		rpa.setConfirmPassword(cpass);
